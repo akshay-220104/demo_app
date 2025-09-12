9290572ab53dbf5e5ce9814db20a6ef92a4ffe04
Palthi <akshay.palthi@capgemini.com>
2025-09-12 12:34:02 +0530

    client-side-pagination

--- demo-frontend/src/app/items/items.component.ts
+++ demo-frontend/src/app/items/items.component.ts
@@ -12,6 +12,8 @@ import { CommonModule } from '@angular/common';
 })
 export class ItemsComponent {
   items$: Observable<Item[]>;
+  currentPage = 1;
+  itemsPerPage = 4;
 
   constructor(private itemsService: ItemsService) {
     this.items$ = this.itemsService.items$;
@@ -19,5 +21,22 @@ export class ItemsComponent {
 
   loadItems() {
     this.itemsService.triggerFetch();
+  }
+  getCurrentPageItems(items: Item[]): Item[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    return items.slice(startIndex, startIndex + this.itemsPerPage);
+  }
+  getTotalPages(items: Item[]): number {
+    return Math.ceil(items.length / this.itemsPerPage);
+  }
+  nextPage(items: Item[]) {
+    if (this.currentPage < this.getTotalPages(items)) {
+      this.currentPage++;
+    }
+  }
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
   }
 }
